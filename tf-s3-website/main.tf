1108cba471dc766a101a356c3712bc67a49c06cb
yoshi108-102 <ginyoku102@gmail.com>
2026-02-21 13:52:07 +0900

    update: add error html

--- tf-s3-website/main.tf
+++ tf-s3-website/main.tf
@@ -43,7 +43,6 @@ resource "aws_s3_bucket_website_configuration" "website" {
 # 公開アクセスブロックを解除（静的ウェブサイトには必要）
 resource "aws_s3_bucket_public_access_block" "website" {
   bucket = aws_s3_bucket.website.id
-
   block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = false
